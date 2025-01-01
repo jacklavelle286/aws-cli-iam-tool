e808138ad30b45306bad32c1f8919262b9235ebe
jacklavelle286 <lavellej286@gmail.com>
2025-01-01 19:00:47 +0000

    update

--- policy.py
+++ policy.py
@@ -1,13 +1,20 @@
 import json
 import boto3
+import os
 
-def create_iam_policy_file(name, sid, effect, service, action, resource):
+# create iam policy functionality
+"""this is the section for creating the iam user"""
+
+def create_iam_policy_file(name, sid, effect, service, action, resource=None):
+    action_field = ["*"] if service == "*" else [f"{service}:{action}"]
     policy_dict = {
         "Sid": sid,
         "Effect": effect,
-        "Action": [f"{service}:{action}"],
-        "Resource": [resource]
+        "Action": action_field
     }
+
+    if resource:
+        policy_dict["Resource"] = [resource]
 
     # Open the template file
     with open("./templates/policy_template.json", mode="r+") as policy_file:
@@ -25,19 +32,15 @@ def create_iam_policy_file(name, sid, effect, service, action, resource):
                 statement["Resource"] = policy_dict["Resource"]
 
         # Dynamically name the new policy file using the `name` argument
-        new_file = f"./output_policies/{name}_policy.json"
+        new_file = f"./output_policies/{name}.json"
 
         # Write the updated policy to the new file
         with open(new_file, mode="w") as new_policy_file:
             json.dump(policy_data, new_policy_file, indent=4)
-            print(f"Policy file created: {new_file}")
+            print(f"Policy file created: {new_file}. \n Creating policy in AWS...")
 
     return new_file  # Return the new file name
 
-
-def delete_iam_policy_file():
-    pass
-    # logic to delete the policy file if there is some kind of error when creating it in AWS so dodgy templates aren't hanging around
 
 
 def create_policy(new_policy):
@@ -52,30 +55,56 @@ def create_policy(new_policy):
             PolicyName=policy_name,
             PolicyDocument=policy_contents,  # JSON string of the policy
         )
-        print(f"Policy created successfully! ARN: {response['Policy']['Arn']}")
+        arn = get_iam_policy_arn(new_policy=new_policy)
+        print(f"Policy created successfully!: {arn}")
     except iam_client.exceptions.MalformedPolicyDocumentException as e:
         print(f"Malformed policy document: in {policy_name}", e)
+        delete_policy_file(new_policy=new_policy)
     except iam_client.exceptions.EntityAlreadyExistsException as e:
         print(f"Policy called {policy_name} already exists:", e)
+        delete_policy_file(new_policy=new_policy)
     except Exception as e:
         print(f"Error creating policy {policy_name}:", e)
+        delete_policy_file(new_policy=new_policy)
+
+def get_iam_policy_arn(new_policy):
+    iam_client = boto3.client("iam")
+    policy_name = new_policy.split("/")[-1].split(".")[0]
+    paginator = iam_client.get_paginator('list_policies')
+    for page in paginator.paginate(Scope="All"):
+        for policy in page['Policies']:
+            if policy['PolicyName'] == policy_name:
+                return policy['Arn']
+
+def delete_policy_file(new_policy):
+    os.remove(new_policy)
+    print(f"policy succesfully locally deleted: {new_policy}")
 
 
+# fix this so it skips the addition of the service if the user enters * for serivce
 
 def get_user_input_policy():
     output = []
-    name = input("Enter your policy name: ").lower()
-    output.append(name)
-    sid = input("Give me your SID: ")
-    output.append(sid)
-    effect = input("Give me your effect (Allow or Deny): ").capitalize()
-    output.append(effect)
-    service = input("Give me your service (e.g., s3): ")
-    output.append(service)
-    action = input("Give me your action (e.g., GetObject): ")
-    output.append(action)
-    resource = input("Give me your resource ARN: ")
-    output.append(resource)
+    while True:
+        name = input("Enter your policy name: ").lower()
+        output.append(name)
+        sid = input("Give me your SID: ")
+        output.append(sid)
+        effect = input("Give me your effect (Allow or Deny): ").capitalize()
+        output.append(effect)
+        service = input("Give me your service (e.g., s3): ")
+        output.append(service)
+        if service == ("*"):
+            break
+        else:
+            output.append(action)
+            resource = input("Give me your resource ARN: ")
+            output.append(resource)
+            if service == "*":
+                wild_card = output[4]
+                output.remove(wild_card)
+        break
+    print(output)
     return output
 
 
--- trustpolicy.py
+++ trustpolicy.py
@@ -1,6 +1,2 @@
 # create trust policy function
 
-
-def create_trust_policy():
-    pass
-# Main script
