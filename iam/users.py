a14013e6161a4f39d256b7e5bef317acfd5dc4a2
jacklavelle286 <lavellej286@gmail.com>
2025-01-12 14:34:53 +0000

    COMMIT_MESSAGE

--- iam/users.py
+++ iam/users.py
@@ -26,6 +26,7 @@ iam_client = boto3.client("iam")
 # revoke access keys
 # assign MFA
 # rotate access keys
+# add extensive error handling
 
 
 def list_iam_users():
@@ -58,56 +59,87 @@ def list_access_keys(username):
     return key_ids
 
 
-
 def list_certificate_ids(username):
     certificates = iam_client.list_signing_certificates(UserName=username)
     cert_ids = [cert['CertificateId'] for cert in certificates.get('Certificates', [])]
     return cert_ids
 
+def list_public_ssh_keys(username):
+    keys = iam_client.list_ssh_public_keys(UserName = username)
+    key_ids = [key['SSHPublicKeyId'] for key in keys.get('SSHPublicKeys', [])]
+    return key_ids
 
+
+def list_service_specific_creds(username):
+    creds = iam_client.list_service_specific_credentials(UserName = username)
+    cred_ids = [cred['ServiceSpecificCredentialId'] for cred in creds.get('ServiceSpecificCredentials', [])]
+    return cred_ids
+
+
+def list_mfa_devices(username):
+    devices = iam_client.list_mfa_devices(UserName=username)
+    device_ids = [device['SerialNumber'] for device in devices.get('MFADevices', [])]
+    return device_ids
+
+def list_groups_for_user(username):
+    groups = iam_client.list_groups_for_user(UserName=username)
+    return [group['GroupName'] for group in groups.get("Groups", [])]
 
 def delete_iam_user(username):
-    # list all attached policies
     print("Deleting attached policies...")
     policies = list_attached_user_policies(username=username, managed=True)
     for policy in policies:
-        print(policy)
-    # get arn for each policy
+        print(f"Deleting: {policy}")
     policy_arns = []
     for name in policies:
         arn = iam_policy.get_iam_policy_arn(name)
         policy_arns.append(arn)
-    # detach user policies
-
     for arn in policy_arns:
         iam_client.detach_user_policy(UserName=username, PolicyArn=arn)
-    # detach Inline policies(DeleteUserPolicy)
-    # get inline policies:
+    print("Deleting inline policies...")
     inline_policies = list_attached_user_policies(username=username, managed=False)
     for i_policy in inline_policies:
-        print(i_policy)
+        print(f"Deleting: {i_policy}")
         iam_client.delete_user_policy(PolicyName=i_policy,UserName=username)
     print("Deleting access keys...")
-    # Access keys(DeleteAccessKey)
-    #list access keys
     key_list = list_access_keys(username)
     for key in key_list:
-        print(key)
+        print(f"Deleting: {key}")
         iam_client.delete_access_key(UserName=username, AccessKeyId=key)
     print("Deleting certificates..")
-    # certificate(DeleteSigningCertificate)
     cert_ids = list_certificate_ids(username)
     for cert in cert_ids:
-        print(cert)
+        print(f"Deleting: {cert}")
         iam_client.delete_signing_certificate(UserName=username, CertificateId=cert)
-    # SSH publickey(DeleteSSHPublicKey)
-    # Git credentials(DeleteServiceSpecificCredential)
-    # Multi - factor authentication(MFA)device(DeactivateMFADevice, DeleteVirtualMFADevice)
-    # Password(DeleteLoginProfile)
-    #iam_client.delete_login_profile(UserName=username)
-    # Inline policies(DeleteUserPolicy)
-    # Group emberships(RemoveUserFromGroup)
-    # delete user:
-    iam_client.delete_user
+    print("Deleting Public SSH Keys...")
+    keys = list_public_ssh_keys(username=username)
+    for key in keys:
+        print(key)
+        print(f"Deleting: {key}")
+        iam_client.delete_ssh_public_key(UserName=username, SSHPublicKeyId=key)
+    print("Deleting Public Git Credentials...")
+    creds = list_service_specific_creds(username)
+    for cred in creds:
+        print(f"Deleting: {cred}")
+        iam_client.delete_service_specific_credential(UserName=username, ServiceSpecificCredentialId=cred)
+    print("Deleting MFA Devices...")
+    devices_ids = list_mfa_devices(username)
+    for id in devices_ids:
+        print(f"Deactivating: {id}")
+        iam_client.deactivate_mfa_device(UserName=username, SerialNumber=id)
+    # for id in devices_ids:
+    #     print(f"Deleting: {id}")
+    #     iam_client.delete_virtual_mfa_device(SerialNumber=id)
+    print("Deleting Login Profile...")
+    iam_client.delete_login_profile(UserName=username)
+    users_groups = list_groups_for_user(username)
+    for group in users_groups:
+        print(f"Removing user from group: {group}")
+        iam_client.remove_user_from_group(UserName=username, GroupName=group)
+    # delete user
+    print("Deleting user...")
+    iam_client.delete_user(UserName=username)
+    print("User Deleted!")
+
 
 
